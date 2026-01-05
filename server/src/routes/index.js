1e4bd0f838949634e700b8675f89a3e72d999d0a
Andikasaktih <andikasaktih@gmail.com>
2026-01-05 14:12:54 +0700

    update route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes.js';
+import bookRoutes from './bookRoutes.js';
+import profileRoutes from './profileRoutes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/books', bookRoutes);
+router.use('/profile', profileRoutes);
 
 // Health check
 router.get('/health', (req, res) => {
